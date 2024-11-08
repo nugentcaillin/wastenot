802186f18882527d92cd4432339a4abc2665edd9
Caillin Nugent <nugentcaillin@gmail.com>
2024-11-08 19:53:31 +1100

    spring init

---
+++ wastenot-backend/src/test/java/com/example/wastenot_backend/WastenotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wastenot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WastenotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
